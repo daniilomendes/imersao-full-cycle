2005bc7a4aadec9b9e3eb9bfb64a2aa2f60d00fb
Danilo Mendes <danilo.mirandha@gmail.com>
2024-06-22 18:09:16 -0300

    api de parceiros com nest.js

---
+++ nestjs-partners-api/README.md
@@ -0,0 +1,41 @@
+# Imersão Full Stack & FullCycle - Sistema de Venda de Ingressos
+
+## Descrição
+
+Repositório da API feita com Nest.js (Reserva de Ingressos)
+
+## Rodar a aplicação
+
+Dentro da pasta `nest` execute o comando abaixo para rodar os containers `Docker`:
+```
+docker compose up
+```
+
+Quando os containers estiverem prontos, precisamos acessar o container do `app` e executar a aplicação:
+
+```
+// entrar no container:
+docker compose exec app bash
+
+// instalar as dependências:
+npm install
+
+// executar a migração do primeiro parceiro:
+npm run migrate:partner1
+
+// executar a migração do segundo parceiro:
+npm run migrate:partner2
+
+// Executar o partner1 na porta 3000
+npm run start:dev
+
+// Executar o partner2 na porta 3001
+npm run start:dev partner2
+
+```
+
+Espere os logs verdinhos do Nest para verificar se deu certo.
+
+* Acessar o arquivo './partner1.http' para criar / listar os eventos, reservar lugares e comprar ingressos do Parceiro 1.
+
+* Acessar o arquivo './partner2.http' para criar / listar os eventos, reservar lugares e comprar ingressos do Parceiro 2.
---
+++ nestjs-partners-api/apps/partner2/src/eventos/eventos.controller.ts
@@ -0,0 +1,81 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  UseGuards,
+} from '@nestjs/common';
+import { EventsService } from '@app/core/events/events.service';
+import { CriarEventoRequest } from './request/criar-evento.request';
+import { AtualizarEventoRequest } from './request/atualizar-evento.request';
+import { ReservarLugarRequest } from './request/reservar-lugar.request';
+import { TicketKind } from '@prisma/client';
+import { AuthGuard } from '@app/core/auth/auth.guard';
+import { ReservarLugarResponse } from './response/reservar-lugar.response';
+
+@Controller('eventos')
+export class EventosControllers {
+  constructor(private readonly eventsService: EventsService) {}
+
+  @Post()
+  create(@Body() criarEventoRequest: CriarEventoRequest) {
+    return this.eventsService.create({
+      name: criarEventoRequest.nome,
+      description: criarEventoRequest.descricao,
+      date: criarEventoRequest.data,
+      price: criarEventoRequest.preco,
+    });
+  }
+
+  @Get()
+  findAll() {
+    return this.eventsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.eventsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() atualizarEventoRequest: AtualizarEventoRequest,
+  ) {
+    return this.eventsService.update(id, {
+      name: atualizarEventoRequest.nome,
+      description: atualizarEventoRequest.descricao,
+      date: atualizarEventoRequest.data,
+      price: atualizarEventoRequest.preco,
+    });
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.eventsService.remove(id);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post(':id/reservar')
+  async reserveSpots(
+    @Body() reservarLugarRequest: ReservarLugarRequest,
+    @Param('id') eventId: string,
+  ) {
+    console.log(reservarLugarRequest);
+    const tickets = await this.eventsService.reserveSpot({
+      eventId,
+      spots: reservarLugarRequest.lugares,
+      ticket_kind:
+        reservarLugarRequest.tipo_ingresso === 'inteira'
+          ? TicketKind.full
+          : TicketKind.half,
+      email: reservarLugarRequest.email,
+    });
+    return new ReservarLugarResponse(tickets);
+  }
+}
